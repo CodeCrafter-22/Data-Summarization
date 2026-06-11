41103890e676cdd9c789f787dacbc84406c59e15
Mansi Bagul <bagulms05@gmail.com>
2026-06-11 16:46:34 +0530

    Updated app and deployment configuration

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import requests
 import os
 from flask import request as req
 
+from dotenv import load_dotenv
+load_dotenv()
+
+os.getenv("HF_TOKEN")
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
@@ -12,7 +17,7 @@ def Index():
 @app.route("/Summarize", methods=["GET", "POST"])
 def Summarize():
     if req.method == "POST":
-        API_URL = "https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6"
+        API_URL = "https://router.huggingface.co/hf-inference/models/sshleifer/distilbart-cnn-12-6"
         headers = {
             "Authorization": f"Bearer {os.environ['HF_TOKEN']}",
         }
@@ -22,6 +27,7 @@ def Summarize():
         minL = maxL//4
 
         def query(payload):
+            
             response = requests.post(API_URL, headers=headers, json=payload)
             return response.json()
 
